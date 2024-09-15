cbaa64d237940c32cbe744366859a4b9a1a6afc3
Lucie Camanez <44371566+TrueCactus@users.noreply.github.com>
2024-09-15 23:33:54 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -12,7 +12,10 @@ secure_app(ui,enable_admin = FALSE,
            
            tags_top =
              tags$div(
-               tags$h4("WELCOME !", style = "align:center"),
+               tags$h4("WELCOME !"),
+               tags$h5("The differents users are - appcreator /appcreatorpwd
+   - example1nav or example1dash / example1pwd -
+   - exampletitanic/exampletitanicpwd"),
                tags$img(
                  src = "https://avatars.githubusercontent.com/u/44371566?v=4", width = 150
                )
